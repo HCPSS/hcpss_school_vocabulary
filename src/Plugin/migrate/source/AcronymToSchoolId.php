3cb7d851ad1b457da9547a8e238e87a3ab62f1a3
Brendan Anderson <brendan_anderson@hcpss.org>
2023-11-20 15:07:05 -0500

    Create AcronymToSchoolId.php

---
+++ src/Plugin/migrate/source/AcronymToSchoolId.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\ikonn_default_content\Plugin\migrate\process;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @MigrateProcessPlugin(
+ *   id = "schools_by_acronym",
+ *   handle_multiples = TRUE
+ * )
+ *
+ * @code
+ * field_parts:
+ *   plugin: schools_by_acronym
+ *   source: acronym
+ * @endcode
+ */
+class SchoolsByAcronym extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entityTypeManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $ids = $this->entityTypeManager
+      ->getStorage('taxonomy_term')
+      ->getQuery()
+      ->condition('vid', 'schools')
+      ->condition('status', TRUE)
+      ->accessCheck(FALSE)
+      ->execute();
+
+    return array_reduce($ids, function ($carry, $item) {
+      $carry[] = ['target_id' => $item];
+      return $carry;
+    });
+  }
+}
